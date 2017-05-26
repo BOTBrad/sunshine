e743c586f8e0e3548ec7aa6de1ab84f0c93e1365
christian w <i@hate.coffee>
2017-05-25 20:13:46 -0400

    create a board

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+struct Board {
+  width: i32,
+  height: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+  let board = Board {
+    width: 20,
+    height: 20,
+  };
+
+  let mut wall = "".to_string();
+
+  for _ in 0..(board.width + 2) {
+    wall.push('#');
+  }
+
+  println!("{}", wall);
+  for _ in 0..board.height {
+    print!("#");
+    for _ in 0..board.width {
+      print!(" ");
+    }
+    print!("#\n");
+  }
+  println!("{}", wall);
 }
