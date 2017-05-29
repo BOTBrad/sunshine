8190524cb96c201bc08d77839aac4deab0b199b3
christian w <i@hate.coffee>
2017-05-29 17:42:01 -0400

    better map collision

--- src/game.rs
+++ src/game.rs
@@ -1,3 +1,5 @@
+use std::vec::Vec;
+
 use graphics;
 use opengl_graphics as gl_gfx;
 use piston::input;
@@ -84,16 +86,75 @@ impl State {
     self.hero.walk(ctl.dpad.flatten());
     let tiles = &self.world.tiles;
 
-    for x in 0..tiles.len() {
-      for y in 0..tiles[x].len() {
-        if let world::Tile::Floor = tiles[x][y] {
+    // figure out the wall status around us
+    let mut walls = [[false, false], [false, false]];
+    let x = self.hero.pos[0].floor() as i32;
+    let y = self.hero.pos[1].floor() as i32;
+    for i in 0..2 {
+      if x + i < 0 || x + i >= tiles.len() as i32 {
+        continue;
+      }
+
+      let col = &tiles[(x + i) as usize];
+
+      for j in 0..2 {
+        if y + j < 0 || y + j >= col.len() as i32 {
           continue;
         }
 
-        let push = physics::collide(&self.hero, &[x as f64, y as f64, 1.0, 1.0]);
-        if let Some(f) = push {
-          self.hero.push(f);
+        if let world::Tile::Wall = col[(y + j) as usize] {
+          walls[i as usize][j as usize] = true;
         }
+      }
+    }
+
+    // construct wall hitboxes (overlapping is ok and actually desirable)
+    let mut wall_boxes = Vec::<physics::HitBox>::with_capacity(4);
+    let xf = x as f64;
+    let yf = y as f64;
+    if walls[0][0] {
+      if walls[1][0] {
+        wall_boxes.push([xf, yf, 2.0, 1.0]);
+      }
+
+      if walls[0][1] {
+        wall_boxes.push([xf, yf, 1.0, 2.0]);
+      }
+
+      if !walls[1][0] && !walls[0][1] {
+        wall_boxes.push([xf, yf, 1.0, 1.0]);
+      }
+    }
+
+    if walls[1][1] {
+      if walls[1][0] {
+        wall_boxes.push([xf + 1.0, yf, 1.0, 2.0]);
+      }
+
+      if walls[0][1] {
+        wall_boxes.push([xf, yf + 1.0, 2.0, 1.0]);
+      }
+
+      if !walls[1][0] && !walls[0][1] {
+        wall_boxes.push([xf + 1.0, yf + 1.0, 1.0, 1.0]);
+      }
+    }
+
+    if !walls[0][0] && !walls[1][1] {
+      if walls[1][0] {
+        wall_boxes.push([xf + 1.0, yf, 1.0, 1.0]);
+      }
+
+      if walls[0][1] {
+        wall_boxes.push([xf, yf + 1.0, 1.0, 1.0]);
+      }
+    }
+
+    // actually collide
+
+    for b in wall_boxes {
+      if let Some(f) = physics::collide(&self.hero, &b) {
+        self.hero.push(f);
       }
     }
   }
